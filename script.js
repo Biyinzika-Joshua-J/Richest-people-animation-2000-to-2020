de3275d9b09868a7e2ef470305d7f1c37101194f
Joshua Joseph <mpaasadebora@gmail.com>
2021-11-14 13:34:40 +0100

    small edit to script.js

--- script.js
+++ script.js
@@ -3,63 +3,55 @@ import { CreateAnimations } from "./createAnimations.js";
 
 let theRichestMen = [...data];
 let richestContainer = document.querySelector(".richest_list");
-let year = document.querySelector('.year');
-const timer = ms => new Promise(res => setTimeout(res, ms));
+let year = document.querySelector(".year");
+let playAnimationBtn = document.getElementById("play");
+
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 let interval;
 
 let animations = CreateAnimations(theRichestMen);
-console.log(animations);
-
-function animate(data,i,j, currentyear){
 
 
-    
-   
-    year.textContent = `${currentyear}`
-    richestContainer.innerHTML = null;
-    for (let t = 0; t < data[i][j].length; t++) {
-      let name = data[i][j][t].name;
-      let networth = data[i][j][t].netwoth;
-      let position = data[i][j][t].position;
-      let image = data[i][j][t].image;
-      let nation = data[i][j][t].nation;
+function animate(data, i, j, currentyear) {
+  year.textContent = `${currentyear}`;
+  richestContainer.innerHTML = null;
+  for (let t = 0; t < data[i][j].length; t++) {
+    let name = data[i][j][t].name;
+    let networth = data[i][j][t].netwoth;
+    let position = data[i][j][t].position;
+    let image = data[i][j][t].image;
+    let nation = data[i][j][t].nation;
 
-      let richMan = `<div class='position'>${position}</div>  <div class="person_worth" style="width:${networth}%">
+    let richMan = `<div class='position'>${position}</div>  <div class="person_worth" style="width:${networth}%">
                                           <h3 class='name'>${name}</h3>
                                       </div>
                                           <img class="avatar" src="${image}" alt="image">
                                           <img class="flag" src="${nation}" alt="nation">
                                           <h3 class='money'>$${networth} billion</h3>`;
 
-      let richManContainer = document.createElement("div");
-      richManContainer.className = "person";
-      richManContainer.innerHTML = richMan;
+    let richManContainer = document.createElement("div");
+    richManContainer.className = "person";
+    richManContainer.innerHTML = richMan;
 
-      richestContainer.appendChild(richManContainer);
-     
+    richestContainer.appendChild(richManContainer);
   }
-
 }
 
-async  function displayTheRichestPeopleData (data) {
+async function displayTheRichestPeopleData(data) {
   let currentYear = 2000;
-
 
   for (let i = 0; i < data.length; i++) {
     for (let j = 0; j < data[i].length; j++) {
       await timer(100);
-     animate(data,i,j, currentYear)
-    
-      
-  }
+      animate(data, i, j, currentYear);
+    }
 
-  currentYear++;
+    currentYear++;
+  }
 }
 
+function playAnimations() {
+  displayTheRichestPeopleData(animations);
+}
 
-};
-
-displayTheRichestPeopleData(animations);
-
-
-
+playAnimationBtn.addEventListener("click", playAnimations);
